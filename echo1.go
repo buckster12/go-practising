f4a1d3edfc5d8ecfcff49e2b6a292b5e9c85d0f2
buckster12 <roboks@mail.ru>
2019-10-21 19:16:25 +0300

    a new program echo2 was added

--- echo1.go
+++ echo1.go
@@ -1,15 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
-}
+// func main() {
+// 	var s, sep string
+// 	for i := 1; i < len(os.Args); i++ {
+// 		s += sep + os.Args[i]
+// 		sep = " "
+// 	}
+// 	fmt.Println(s)
+// }
