28e4bc5db11243015bf1f3a0eed5f8967368e855
buckster12 <roboks@mail.ru>
2019-10-21 19:49:49 +0300

    a few new versions of echo program

--- echo2.go
+++ echo2.go
@@ -6,10 +6,20 @@ import (
 )
 
 func main() {
-	s, sep := "", ""
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
-		sep = " "
+	// s, sep := "", ""
+	// for _, arg := range os.Args[1:] {
+	// s += sep + arg
+	// sep = " "
+	// }
+	// fmt.Println(s)
+
+	// fmt.Println(strings.Join(os.Args[1:], "-"))
+
+	// fmt.Println(os.Args[:])
+
+	for key, value := range os.Args {
+		fmt.Println(key)
+		fmt.Println(value)
+		fmt.Println("---")
 	}
-	fmt.Println(s)
 }
